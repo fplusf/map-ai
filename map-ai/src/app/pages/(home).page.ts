4facfddfe4f0da8c8ad2f1a1dca8547f45e4a030
Farukh Fuzaylov <fuzailof@gmail.com>
2025-01-30 00:03:25 +0800

    feat: project scaffold and sample app with supabase connected

--- map-ai/src/app/pages/(home).page.ts
+++ map-ai/src/app/pages/(home).page.ts
@@ -4,11 +4,8 @@ import { AnalogWelcomeComponent } from './analog-welcome.component';
 
 @Component({
   selector: 'map-ai-home',
-  
+
   imports: [AnalogWelcomeComponent],
-  template: `
-     <map-ai-analog-welcome/>
-  `,
+  template: ` <map-ai-analog-welcome /> `,
 })
-export default class HomeComponent {
-}
+export default class HomeComponent {}
--- map-ai/src/server/trpc/routers/notes.ts
+++ map-ai/src/server/trpc/routers/notes.ts
@@ -1,9 +1,8 @@
+import { eq } from 'drizzle-orm';
 import { z } from 'zod';
+import { db, notes } from '../../../db';
 import { publicProcedure, router } from '../trpc';
-import { Note } from '../../../note';
 
-let noteId = 0;
-const notes: Note[] = [];
 export const noteRouter = router({
   create: publicProcedure
     .input(
@@ -11,22 +10,16 @@ export const noteRouter = router({
         note: z.string(),
       })
     )
-    .mutation(({ input }) =>
-      notes.push({
-        id: noteId++,
-        note: input.note,
-        createdAt: new Date().toISOString(),
-      })
-    ),
-  list: publicProcedure.query(() => notes),
+    .mutation(async ({ input }) => await db.insert(notes).values({ note: input.note }).returning()),
+  list: publicProcedure.query(async () => {
+    const selectedNotes = await db.select().from(notes);
+    return selectedNotes.map(note => ({ ...note, id: +note.id }));
+  }),
   remove: publicProcedure
     .input(
       z.object({
         id: z.number(),
       })
     )
-    .mutation(({ input }) => {
-      const index = notes.findIndex((note) => input.id === note.id);
-      notes.splice(index, 1);
-    }),
+    .mutation(async ({ input }) => await db.delete(notes).where(eq(notes.id, input.id)).returning()),
 });
